901a37c11ff2c8d088dbc24b1ccc519ba8efe24c
unsubstantiated-Script <jusfulton@gmail.com>
2022-07-08 07:37:01 -0700

    Project complete

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 # Create your models here.
+from django.utils.text import slugify
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    # db_index makes searching the field faster id out of the box has index search
+    slug = models.SlugField(default="", null=False, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     # This is needed to print data to the console vs.
     def __str__(self):
--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index )
+    path('', views.index ),
+    path('<slug:slug>', views.book_detail, name="book-detail")
 ]
