69b99126b3ecf239802c3be7536465ac61ce114a
unsubstantiated-Script <jusfulton@gmail.com>
2022-07-09 19:52:46 -0700

    book proj done

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author, Address, Country
 # Register your models here.
 
 # Getting more control over models in admin with an admin class
@@ -15,5 +15,21 @@ class BookAdmin(admin.ModelAdmin):
     # Adding more cols/headers to table in admin
     list_display = ("title", "author",)
 
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("first_name", "last_name",)
+    # Adding more cols/headers to table in admin
+    list_display = ("first_name", "last_name",)
+
+class AddressAdmin(admin.ModelAdmin):
+    list_filter = ("street", "postal_code", "city",)
+    # Adding more cols/headers to table in admin
+    list_display = ("street", "postal_code", "city",)
+
+
 # basics for getting model into admin
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country)
+
